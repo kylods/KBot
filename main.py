b2c06df077c69aac33e3b2582c0a91087ccf13bc
Kyle Dvorak <kylodss@gmail.com>
2023-10-03 06:33:27 -0500

    WE GOT MUSIC WORKING BABYYYYY

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import os
-import discord
-from discord.ext import commands
 import config
 import logging
 import json
+import discord
+from discord.ext import commands
+from yt_dlp import YoutubeDL
+import asyncio
+from datetime import timedelta
 
 handler = logging.basicConfig(level=logging.INFO,
                               format='[%(asctime)s] %(levelname)s:%(name)s: %(message)s', # Formats each log line
@@ -15,6 +18,35 @@ intents = discord.Intents.default()
 intents.message_content = True
 intents.members = True
 
+if not os.path.exists('downloads'):
+    os.makedirs('downloads')
+
+ytdl_opts = {'logger': handler,
+             'format': 'bestaudio[abr=128]/bestaudio/best',  # Prioritize 128kbps audio
+             'outtmpl': 'downloads/%(title)s.%(ext)s',  # Downloaded files will be saved in a 'downloads' folder
+}
+
+ytdl = YoutubeDL(ytdl_opts)
+
+class YTDLSource(discord.PCMVolumeTransformer):
+    def __init__(self, source, *, data):
+        super().__init__(source)
+        self.data = data
+        self.title = data.get('title')
+        self.url = data.get('url')
+
+    @classmethod
+    async def from_url(cls, url, *, loop=None):
+        loop = loop or asyncio.get_event_loop()
+        data = await loop.run_in_executor(None, lambda: ytdl.extract_info(url, download=True))
+
+        if 'entries' in data:
+            data = data['entries'][0]
+
+        filename = ytdl.prepare_filename(data)
+        return cls(discord.FFmpegPCMAudio(filename), data=data)
+
+
 class Server():
     def __init__(self):
         self.queue = []
@@ -25,7 +57,8 @@ class Server():
             'vote_skip': True,
             'text_channel': None,
             'voice_channel': None,
-            'dj_role': None
+            'dj_role': None,
+            'skip_percentage': 50,
         }
 
 # in-memory server database
@@ -97,6 +130,75 @@ async def setprefix(ctx, prefix):
     servers[ctx.guild.id].settings['prefix'] = prefix
     save_server_settings(ctx.guild.id)  # Save only the settings of this specific server
     await ctx.send(f"Prefix set to `{prefix}` for this server!")
+
+@bot.hybrid_command()
+async def join(ctx):
+    # Check if the author is in a voice channel
+    if ctx.author.voice and ctx.author.voice.channel:
+        channel = ctx.author.voice.channel
+        permissions = channel.permissions_for(ctx.guild.me)
+        
+        # Check if the bot has the permission to join the voice channel
+        if not permissions.connect:
+            await ctx.send(f"I don't have the permissions to join `{channel.name}`.")
+            return False
+        
+        # Check if the bot is already in a voice channel
+        if ctx.voice_client:
+            await ctx.voice_client.move_to(channel)
+        else:
+            await channel.connect()
+
+        await ctx.send(f"Joined `{channel.name}`!")
+        return True
+    else:
+        await ctx.send("You are not in a voice channel.")
+        return False
+
+@bot.hybrid_command()
+async def leave(ctx):
+    if ctx.voice_client:
+        await ctx.voice_client.disconnect()
+
+@bot.hybrid_command()
+async def play(ctx, url):
+    if not ctx.voice_client:
+        success = await ctx.invoke(join)  # Join user's voice channel if not in one
+        if not success:
+            return
+
+    async with ctx.typing():
+        player = await YTDLSource.from_url(url, loop=bot.loop)
+    #    clen = str(player.data.get('duration'))
+    #    player.url += '&range=0-' + clen # This fixes youtube throttling download speed. dont ask me why this works
+        ctx.voice_client.play(player, after=lambda e: song_finished(ctx, e))
+        await ctx.send(f"Playing: **{player.title}**")
+
+        if ctx.guild.id not in servers:
+            servers[ctx.guild.id] = Server()
+        servers[ctx.guild.id].nowplaying = {
+            'title': player.title,
+            'url': url,
+            'length': timedelta(seconds = player.data.get('duration'))  # this gets the duration from the ytdl data
+        }
+        
+        await ctx.send(f"Playing: **{player.title}**")
+
+def song_finished(ctx, error):
+    if error:
+        print(f"Player error: {error}")
+
+    # Clear the nowplaying variable when the song finishes
+    if ctx.guild.id in servers:
+        servers[ctx.guild.id].nowplaying = {}
+
+@bot.hybrid_command()
+async def nowplaying(ctx):
+    if ctx.guild.id in servers and servers[ctx.guild.id].nowplaying:
+        np = servers[ctx.guild.id].nowplaying
+        await ctx.send(f"Currently playing: **{np['title']}**\nURL: {np['url']}\nLength: {np['length']}")
+    else:
+        await ctx.send("No song is currently playing.")
 
 def main():
     bot.run(config.token, log_handler=handler)
